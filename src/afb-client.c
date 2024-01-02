40c8f92b79c80f9a73d074bce9c7ab9850e6f51a
José Bollo <jose.bollo@iot.bzh>
2024-01-02 15:16:46 +0100

    Update copyright notices for 2024

--- src/afb-client.c
+++ src/afb-client.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2023 IoT.bzh Company
+ * Copyright (C) 2015-2024 IoT.bzh Company
  * Author "Fulup Ar Foll"
  * Author: José Bollo <jose.bollo@iot.bzh>
  *
@@ -189,7 +189,7 @@ static void version(char *arg0)
 	char *name = strrchr(arg0, '/');
 	name = name ? name + 1 : arg0;
 
-	printf("\n%s %s\nCopyright (C) 2015-2023 IoT.bzh Company\n\n", name, VERSION);
+	printf("\n%s %s\nCopyright (C) 2015-2024 IoT.bzh Company\n\n", name, VERSION);
 	exit(0);
 }
 
